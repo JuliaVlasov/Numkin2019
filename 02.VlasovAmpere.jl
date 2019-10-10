6bd4eac3a325d4e9c859f582eccc34dc28db927a
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-10 17:22:42 +0200

    Update 02.VlasovAmpere.jl

--- 02.VlasovAmpere.jl
+++ 02.VlasovAmpere.jl
@@ -1,41 +1,22 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,jl:light
-#     text_representation:
-#       extension: .jl
-#       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.2.4
-#   kernelspec:
-#     display_name: Julia 1.2.0
-#     language: julia
-#     name: julia-1.2
-# ---
-
-# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # ## 1D1V Vlasov–Ampere system
 #
-# $$
-# \frac{\partial f}{\partial t} + \upsilon \frac{\partial f}{\partial x}
-# - E(t,x) \frac{\partial f}{\partial \upsilon} = 0
-# $$
+# ```math
+# \\frac{\\partial f}{\\partial t} + \\upsilon \\frac{\\partial f}{\\partial x} - E(t,x) \\frac{\\partial f}{\\partial \\upsilon} = 0
+# ```
 #
-# $$
-# \frac{\partial E}{\partial t} = - J = \int f\upsilon \; d\upsilon
-# $$
+# ```math
+# \\frac{\\partial E}{\\partial t} = - J = \\displaystyle \\int  f\\upsilon \\; d\\upsilon
+# ```
 
-# + {"slideshow": {"slide_type": "slide"}}
+# ---
+
 using ProgressMeter, FFTW, Plots, LinearAlgebra
 using BenchmarkTools, Statistics
 
-# + {"slideshow": {"slide_type": "slide"}}
 """
     UniformMesh(start, stop, length)
 
-1D uniform mesh data. This is a periodic domain so the end point is 
-removed
+1D uniform mesh data for periodic domain (end point is removed)
 """
 struct UniformMesh
 
@@ -56,10 +37,12 @@ struct UniformMesh
 
 end
 
+# ---
+#
+# ## Compute charge density ρ(x)
+#
 
-# + {"slideshow": {"slide_type": "slide"}}
 """
-
     compute_rho( mesh, f)
 
 Compute charge density ρ(x,t) = ∫ f(x,v,t) dv
@@ -75,8 +58,14 @@ function compute_rho(meshv::UniformMesh, f)
    
 end
 
-# + {"slideshow": {"slide_type": "slide"}}
+# ---
+#
+# ## Compute electric field from ρ(x)
+#
+
 """
+    compute_e(mesh, ρ)
+
 compute electric field from ρ
 """
 function compute_e(mesh::UniformMesh, ρ)
@@ -89,39 +78,18 @@ function compute_e(mesh::UniformMesh, ρ)
 
 end
 
-
-# + {"slideshow": {"slide_type": "slide"}}
+#
+# ---
+#
+# ## Callable struct `Advection`
+#
 """
     advection! = AmpereAdvection( mesh, kx)
-
-    ∂f/∂t − v ∂f/∂x  = 0
-    ∂E/∂t = −J = ∫ fv dv
-    ∂f/∂t − E(x) ∂f/∂v  = 0
 
 For every created struct, two methods are available
 - Advection method along v
 - Advection method along x and e computation
 
-## Algorithm to compute electric field during advection along x
-
-- For each ``j`` compute discrete Fourier transform in ``x``
- of ``f^n(x_i,\\upsilon_j)`` yielding ``f_k^n(\\upsilon_j)``, 
-- For `` k \\neq 0 ``, compute 
-```math
-f^{n+1}_k(\\upsilon_j) = e^{−2i\\pi k \\upsilon
-    \\Delta t/L} f_n^k(\\upsilon_j),
-```
-```math
-\\rho_k^{n+1} = \\Delta \\upsilon \\sum_j􏰄 f^{n+1}_k(\\upsilon_j),
-```
-```math
-E^{n+1}_k = \\rho^{n+1}_k L/(2i\\pi k \\epsilon_0),
-```
-- For ``k = 0`` do nothing: 
-```math
-f_{n+1}(\\upsilon_j) = f^n_k(\\upsilon_j), E^{n+1}_k = E^n_k.
-```
-- Perform inverse discrete Fourier transform of ``E^{n+1}_k`` and for each ``j`` of ``f^{n+1}_k (\\upsilon_j)``.
 """
 struct AmpereAdvection 
     
@@ -142,10 +110,7 @@ struct AmpereAdvection
 end
 
 
-# + {"slideshow": {"slide_type": "slide"}}
-?AmpereAdvection
-
-# + {"slideshow": {"slide_type": "slide"}}
+# ---
 
 function (adv :: AmpereAdvection)( fᵗ  :: Array{ComplexF64,2}, 
                                    e   :: Vector{ComplexF64}, 
@@ -156,8 +121,7 @@ function (adv :: AmpereAdvection)( fᵗ  :: Array{ComplexF64,2},
 
 end
 
-
-# + {"slideshow": {"slide_type": "slide"}}
+# --
 
 function (adv :: AmpereAdvection)( f   :: Array{ComplexF64,2}, 
                                    e   :: Vector{ComplexF64}, 
@@ -171,54 +135,71 @@ function (adv :: AmpereAdvection)( f   :: Array{ComplexF64,2},
     dv = v[2]-v[1]
     ρ = dv * vec(sum(f,dims=2))  
     for i in 2:length(e)
-        e[i] = -1im * ρ[i] ./ adv.kx[i]
+        e[i] = -1im * ρ[i] / adv.kx[i]
     end
     e[1] = 0.0
-    ifft!(f,1)
+    ifft!(f, 1)
     ifft!(e)
     e .= real(e)
 end
 
-
-# + {"slideshow": {"slide_type": "slide"}}
+#
+#
+# ----
+#
+# ## Initial distribution function
+#
+# ```math
+# f(x,v) = \\frac{1}{\\sqrt{2\\pi}}(1+ ϵ \\cdot cos(k_x x)) e^{-v^2/2}
+# ```
+#
 """
+    landau( ϵ, kx, x, v )
+
 Landau damping initialisation function
 
 [Wikipedia](https://en.wikipedia.org/wiki/Landau_damping)
 
-```math
-f(x,v) = \\frac{1}{\\sqrt{2\\pi}}(1+ ϵ \\cdot cos(kₓ x)) e^{-v^2/2}
-```
 
 """
 function landau( ϵ, kx, x, v )
     
-    (1.0.+ϵ*cos.(kx*x))/sqrt(2π) .* transpose(exp.(-0.5*v.*v))
+    (1.0 .+ ϵ*cos.(kx*x))/sqrt(2π) .* transpose(exp.(-0.5*v.*v))
     
 end
 
-# + {"slideshow": {"slide_type": "slide"}}
-?landau
+# ---
 
-# + {"slideshow": {"slide_type": "slide"}}
 nx, nv = 256, 256
 xmin, xmax =  0., 4*π
 vmin, vmax = -6., 6.
 tf = 60
 nt = 600
 meshx = UniformMesh(xmin, xmax, nx)
-meshv = UniformMesh(vmin, vmax, nv)
+meshv = UniformMesh(vmin, vmax, nv);
+
+# --
             
 # Initialize distribution function
 x = meshx.points
 v = meshv.points
 ϵ, kx = 0.001, 0.5
-    
+
 # Allocate arrays for distribution function and its transposed
 f = zeros(Complex{Float64},(nx,nv))
 fᵀ= zeros(Complex{Float64},(nv,nx))
     
-f .= landau( ϵ, kx, x, v)
+f .= landau( ϵ, kx, x, v); 
+
+# ---
+
+# Plot the distribution
+
+surface( x, v, real(f))
+savefig("landau.svg")
+# ![](landau.svg)
+
+# ---
     
 transpose!(fᵀ,f)
     
@@ -233,24 +214,29 @@ dt = tf / nt
 advection_x! = AmpereAdvection( meshx )
 advection_v! = AmpereAdvection( meshv );
 
-# +
+# ---
         
 @showprogress 1 for i in 1:nt
     
-    advection_v!(fᵀ, e,  0.5dt)
+    advection_v!(fᵀ, e, 0.5dt)
     
-    transpose!(f,fᵀ)
+    transpose!(f, fᵀ)
     
     advection_x!( f, e, v, dt)
     
-    push!(mod_e, log(sqrt((sum(e.^2))*meshx.step)))
+    push!(mod_e, log(sqrt((sum(e.^2))*meshx.step))) # diagnostic
     
-    transpose!(fᵀ,f)
+    transpose!(fᵀ, f)
     
-    advection_v!(fᵀ, e,  0.5dt)
+    advection_v!(fᵀ, e, 0.5dt)
+
 end
 
-# + {"slideshow": {"slide_type": "slide"}}
-t =  range(0,stop=tf,length=nt)
+# ---
+
+t = range(0, stop=tf, length=nt)
 plot(t, -0.1533*t.-5.48)
 plot!(t, mod_e , label=:ampere )
+savefig("mod_e.svg")
+
+# ![](mod_e.svg)
