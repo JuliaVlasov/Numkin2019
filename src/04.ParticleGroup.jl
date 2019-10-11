c045094bafb4dc37955851a3236f5b0cf00e21ab
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-11 16:27:29 +0200

    Update 04.ParticleGroup.jl

--- src/04.ParticleGroup.jl
+++ src/04.ParticleGroup.jl
@@ -2,7 +2,8 @@
 #
 # ## The ParticleGroup example
 
-using Distributions, Plots, Sobol, LinearAlgebra
+import Sobol
+using Plots, LinearAlgebra
 
 #     ParticleGroup{D,V}(n_particles, charge, mass)
 #
@@ -88,8 +89,7 @@ end
 # ---
 
 # 
-# Sampling from a probability distribution to initialize
-# a Landau damping
+# Sampling from a probability distribution to initialize Landau damping
 # 
 function landau_sampling!( pg :: ParticleGroup{1,2}, alpha, kx )
     
@@ -104,13 +104,13 @@ function landau_sampling!( pg :: ParticleGroup{1,2}, alpha, kx )
         x1
     end
     
-    s = SobolSeq(2)
+    s = Sobol.SobolSeq(2)
     nbpart = pg.n_particles
 
     for i=1:nbpart
 
         v = sqrt(-2 * log( (i-0.5)/nbpart))
-        r1, r2 = next!(s)
+        r1, r2 = Sobol.next!(s)
         θ = r1 * 2π
         set_x!(pg,  i, newton(r2))
         set_v!(pg,  i, [v * cos(θ), v * sin(θ)])
@@ -126,19 +126,20 @@ alpha, kx = 0.1, 0.5
 landau_sampling!(pg, alpha, kx)
 
 # --
-xp = vcat([get_x(pg,i) for i in 1:pg.n_particles]...)
+
+xp = vcat([get_x(pg, i) for i in 1:pg.n_particles]...)
 vp = vcat([get_v(pg, i) for i in 1:pg.n_particles]'...)
 
 # ---
 
-p = plot(layout=(3,1))
-histogram!(p[1,1], xp, normalize=true, bins = 100, lab=:x)
-plot!(p[1,1], x-> (1+alpha*cos(kx*x))/(2π/kx), 0., 2π/kx, lab="")
-histogram!(p[2,1], vp[:,1], normalize=true, bins = 100, lab=:vx)
-plot!(p[2,1], v-> exp( - v^2 / 2) * 4 / π^2 , -6, 6, lab="")
-histogram!(p[3,1], vp[:,2], normalize=true, bins = 100, lab=:vy)
-plot!(p[3,1], v-> exp( - v^2 / 2) * 4 / π^2 , -6, 6, lab="")
-savefig("particles.svg")
+pp = plot(layout=(3,1))
+histogram!(pp[1,1], xp, normalize=true, bins = 100, lab=:x)
+plot!(pp[1,1], x -> (1+alpha*cos(kx*x))/(2π/kx), 0., 2π/kx, lab="")
+histogram!(pp[2,1], vp[:,1], normalize=true, bins = 100, lab=:vx)
+plot!(pp[2,1], v -> exp( - v^2 / 2) * 4 / π^2 , -6, 6, lab="")
+histogram!(pp[3,1], vp[:,2], normalize=true, bins = 100, lab=:vy)
+plot!(pp[3,1], v -> exp( - v^2 / 2) * 4 / π^2 , -6, 6, lab="")
+savefig("particles.svg"); nothing # hide
 
 # ![](particles.svg)
 
