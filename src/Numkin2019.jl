90c4b07232392a9a9363ca32c7d26d6861dfce01
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-11 22:23:20 +0200

    Update Numkin2019.jl

--- src/Numkin2019.jl
+++ src/Numkin2019.jl
@@ -1,10 +1,5 @@
 module Numkin2019
 
-    include("01.Julia.Introduction.jl")
-    include("02.VlasovAmpere.jl")
-    include("03.RotationGPU.jl")
-    include("04.ParticleGroup.jl")
-    include("05.GEMPIC.jl")
-    include("06.Conclusions.jl")
+    hello( name ) = " hello $name "
 
 end
