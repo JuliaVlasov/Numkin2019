b8c1317b1fb6a14a97b39b4b6edbefa52ceab92b
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-10-11 17:03:59 +0200

    Update 01.Julia.Introduction.jl

--- src/01.Julia.Introduction.jl
+++ src/01.Julia.Introduction.jl
@@ -1,48 +1,48 @@
-# # Who am I ?
-#
-#  - My name is *Pierre Navaro*
-#  - **Fortran 77 + PVM** : during my PhD 1998-2002 (Université du Havre) 
-#  - **Fortran 90-2003 + OpenMP-MPI** : Engineer in Strasbourg (2003-2015) at IRMA
-#  - **Numpy + Cython, R + Rcpp** : engineer in Rennes (2015-now) at IRMAR
-#  - **Julia v1.0** since July 2018 
+#md # # Who am I ?
+#md #
+#md #  - My name is *Pierre Navaro*
+#md #  - **Fortran 77 + PVM** : during my PhD 1998-2002 (Université du Havre) 
+#md #  - **Fortran 90-2003 + OpenMP-MPI** : Engineer in Strasbourg (2003-2015) at IRMA
+#md #  - **Numpy + Cython, R + Rcpp** : engineer in Rennes (2015-now) at IRMAR
+#md #  - **Julia v1.0** since July 2018 
 
 #md # ---
 
-#
-# # Why Julia?
-#
-# - Born in 2009 and version 1.0 was released in August 2018.
-# - High-level languages like Python and R let one explore and experiment rapidly, but can run slow.
-# - Low-level languages like Fortran/C++ tend to take longer to develop, but run fast.
-# - This is sometimes called the "two language problem" and is something the Julia developers set out to eliminate. 
-# - Julia's promise is to provide a "best of both worlds" experience for programmers who need to develop novel algorithms and bring them into production environments with minimal effort.
-#
+#md #
+#md # # Why Julia?
+#md #
+#md # - Born in 2009 and version 1.0 was released in August 2018.
+#md # - High-level languages like Python and R let one explore and experiment rapidly, but can run slow.
+#md # - Low-level languages like Fortran/C++ tend to take longer to develop, but run fast.
+#md # - This is sometimes called the "two language problem" and is something the Julia developers set out to eliminate. 
+#md # - Julia's promise is to provide a "best of both worlds" experience for programmers who need to develop novel algorithms and bring them into production environments with minimal effort.
+#md #
 
 #md # ---
 
-# # Julia's Engineering and Design Tradoffs
-#
-# - Type structures cannot bechanges after created (less dynamism but memory layout can be optimized for)
-# - All functions are JIT compiled via LLVM (interactive lags but massive runtime improvements)
-# - All functions specialize on types of arguments (more compilation but give generic programming structures)
-# - Julia is interactive (use it like Python and R, but makes it harder to get binaries)
-# - Julia has great methods for handling mutation (more optimization opportunities like C/Fortran, but more cognative burden)
-# - Julia's Base library and most packages are written in Julia, (you can understand the source, but learn a new package)
-# - Julia has expensive tooling for code generation and metaprogramming (consise and more optimizations, but some codes can be for experienced users)
-#
-# To me, this gives me a language with a lot of depth which works well for computationally-expensive scientific applications.
-#
-# [© ChrisRackaukas](https://www.youtube.com/watch?v=zJ3R6vOhibA&feature=em-uploademail) 
+#md # # Julia's Engineering and Design Tradoffs
+#md #
+#md # - Type structures cannot bechanges after created (less dynamism but memory layout can be optimized for)
+#md # - All functions are JIT compiled via LLVM (interactive lags but massive runtime improvements)
+#md # - All functions specialize on types of arguments (more compilation but give generic programming structures)
+#md # - Julia is interactive (use it like Python and R, but makes it harder to get binaries)
+#md # - Julia has great methods for handling mutation (more optimization opportunities like C/Fortran, but more cognative burden)
+#md # - Julia's Base library and most packages are written in Julia, (you can understand the source, but learn a new package)
+#md # - Julia has expensive tooling for code generation and metaprogramming (consise and more optimizations, but some codes can be for experienced users)
+#md #
+#md # To me, this gives me a language with a lot of depth which works well for computationally-expensive scientific applications.
+#md #
+#md # [© ChrisRackaukas](https://www.youtube.com/watch?v=zJ3R6vOhibA&feature=em-uploademail) 
 
 #md # ---
 
-# # Type-Dispatch Programming
-#
-# - Centered around implementing the generic template of the algorithm not around building representations of data.
-# - The data type choose how to efficiently implement the algorithm.
-# - With this feature share and reuse code is very easy
-#
-# [JuliaCon 2019 | The Unreasonable Effectiveness of Multiple Dispatch | Stefan Karpinski](https://youtu.be/kc9HwsxE1OY)
+#md # # Type-Dispatch Programming
+#md #
+#md # - Centered around implementing the generic template of the algorithm not around building representations of data.
+#md # - The data type choose how to efficiently implement the algorithm.
+#md # - With this feature share and reuse code is very easy
+#md #
+#md # [JuliaCon 2019 | The Unreasonable Effectiveness of Multiple Dispatch | Stefan Karpinski](https://youtu.be/kc9HwsxE1OY)
 
 #md # ---
 
@@ -111,7 +111,7 @@ u = u₀[2] .* cos.(sqrt(g / L) .* sol.t)
 
 plot(sol.t, getindex.(sol.u, 2), label = "Numerical")
 plot!(sol.t, u, label = "Analytic")
-savefig("pendulum2.svg"); 
+#md savefig("pendulum2.svg"); 
 
 
 #md # ![](pendulum2.svg)
